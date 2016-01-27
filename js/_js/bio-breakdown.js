1e4509d15521a14a049ca22360cb96898ff78b61
Don Denton <don@happycollision.com>
2016-01-27 10:32:03 -0500

    Make Who's who switcher work with window.location
    On load, the switcher will be open to the proper group based on the
    URL. On clicks, the url will change to reflect the current group.
    
    The `id` for the “board” group was removed, since this caused page
    jumping when the switcher was clicked.

--- js/_js/bio-breakdown.js
+++ js/_js/bio-breakdown.js
@@ -1,11 +1,13 @@
 var $bioGroups = $('.bio-group');
 var labels = [];
 var $switcher = $('<nav class="switcher"></nav>');
+var location = window.location;
+console.log(location.hash.split('#')[1]);
 
 $.each( $bioGroups, function(){
   var $thisGroup = $(this);
   var label = $(this).find('.bio-group-label').html();
-  var $switchLink = $('<a>'+label+'</a>');
+  var $switchLink = $('<a class="group-name-' + label.toLowerCase() + '">'+label+'</a>');
   labels.push(label);
   $(this).hide();
 
@@ -14,10 +16,22 @@ $.each( $bioGroups, function(){
     $switchLink.addClass('active');
     $bioGroups.hide();
     $thisGroup.show();
+    window.location.replace('#' + label.toLowerCase());
   });
 
   $switcher.append($switchLink);
 });
 
 $('.bios-page').prepend($switcher);
-$switcher.find('a:first').click();
+
+if (location.hash != ""){
+  var groupName = location.hash.split('#')[1].toLowerCase();
+  var $groupButton = $switcher.find('a.group-name-' + groupName);
+  if ($groupButton.length > 0){
+    $groupButton.click();
+  } else {
+    $switcher.find('a:first').click();
+  }
+} else {
+  $switcher.find('a:first').click();
+}
