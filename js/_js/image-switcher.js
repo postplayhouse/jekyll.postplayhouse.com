660af1c74b99abda23977ee6122819a0402bae8c
Don Denton <don@happycollision.com>
2018-07-31 17:28:45 -0500

    Simple image swapping

--- js/_js/image-switcher.js
+++ js/_js/image-switcher.js
@@ -1,6 +1,33 @@
+(function() {
+  function getBasePath(path) {
+    return path.split('/').slice(0, -1).join('/') + '/';
+  }
 
+  function replaceImageWith(el, url) {
+    el.src = url;
+  }
 
-window.initSwitchImage = (el) => {
-  const list = $(el).data('switch-image-list');
-  console.log({list, el})
-};
+  function getNextImage(current, urls) {
+    var wrappedList = urls.concat(urls[0]);
+    var currentIndex = wrappedList.indexOf(current);
+    return wrappedList[currentIndex + 1];
+  }
+
+  function rotateImages(el, urls) {
+    setInterval(function() {
+      var src = getNextImage(el.src, urls)
+      replaceImageWith(el, src)
+    }, 4000)
+  }
+
+  function prepareUrls(baseUrl, fileNames) {
+    return fileNames.map(function (name) { return baseUrl + name; })
+  }
+
+  window.initSwitchImage = function (el) {
+    var list = $(el).data('switch-image-list').replace(/\n\s+/g, '').split(',')
+      .filter(function(v) { return v !== '' });
+    var urls = prepareUrls(getBasePath(el.src), list);
+    rotateImages(el, urls);
+  };
+})()
